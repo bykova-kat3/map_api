8260502342d4db36e56106b248327993b61feccc
brrr-ali <brisina.a@yandex.com>
2021-03-24 16:46:49 +0700

    5

--- main.py
+++ main.py
@@ -1,73 +1,101 @@
 import os
 import sys
-
 import requests
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from qtpy import QtCore
+from design import Ui_MainWindow
 
-SCREEN_SIZE = [600, 450]
+
+# SCREEN_SIZE = [800, 450]
 
 
-class Example(QWidget):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, req):
         super().__init__()
-        self.getImage(self.resp(req))
+        self.z = 10
+        self.address = req
+        self.setupUi(self)
         self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Отображение карты')
+        self.pixmap = QPixmap(self.getImage(self.response(self.address)))
+        self.image.setPixmap(self.pixmap)
+        self.btn_search.clicked.connect(self.search)
 
     def getImage(self, apiinf):
         response = requests.get(apiinf[0], params=apiinf[1])
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
             file.write(response.content)
+        return map_file
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-        # Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+    def search(self):
+        if self.search_text.text():
+            self.address = self.search_text.text()
+            self.image.setPixmap(QPixmap(self.getImage(self.response(self.address, poss=True))))
+            self.search_text.setText('')
+
+    def keyPressEvent(self, event):
+        fl = 0
+        if event.key() == QtCore.Qt.Key_1:
+            self.z += 1
+            fl = 1
+        if event.key() == QtCore.Qt.Key_2:
+            self.z -= 1
+            fl = 1
+        if self.z < 0:
+            self.z = 0
+        elif self.z > 17:
+            self.z = 17
+        else:
+            fl = 1
+        if fl:
+            self.image.setPixmap(QPixmap(self.getImage(self.response(self.address))))
 
     def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
+        os.remove('map.png')
 
-    def resp(self, reqw):
-        toponym_to_find = reqw
-
+    def response(self, address, poss=False):
+        toponym_to_find = address
         geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
-
         geocoder_params = {
             "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             "geocode": toponym_to_find,
             "format": "json"}
-
         response = requests.get(geocoder_api_server, params=geocoder_params)
-
         if not response:
             pass
-
         json_response = response.json()
         toponym = json_response["response"]["GeoObjectCollection"][
             "featureMember"][0]["GeoObject"]
         toponym_coodrinates = toponym["Point"]["pos"]
+        self.address = toponym_coodrinates
         toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
         toponym_lc = toponym["boundedBy"]["Envelope"]["lowerCorner"]
         toponym_uc = toponym["boundedBy"]["Envelope"]["upperCorner"]
-        map_params = {
-            "ll": ",".join([toponym_longitude, toponym_lattitude]),
-            "spn": ",".join([str(float(toponym_uc.split()[0]) - float(toponym_lc.split()[0])),
-                             str(float(toponym_uc.split()[1]) - float(toponym_lc.split()[1]))]),
-            "l": "map"
-        }
+        if poss:
+            map_params = {
+                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                "l": "map",
+                "spn": ",".join([str(float(toponym_uc.split()[0]) - float(toponym_lc.split()[0])),
+                                 str(float(toponym_uc.split()[1]) - float(toponym_lc.split()[1]))]),
+            }
+        else:
+            map_params = {
+                "ll": ",".join([toponym_longitude, toponym_lattitude]),
+                "l": "map",
+                "z": self.z
+            }
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         return map_api_server, map_params
+
+    sys.excepthook = lambda cls, exception, traceback: sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example(input('Введите адрес '))
+    ex = MainWindow(input('Введите адрес '))
     ex.show()
     sys.exit(app.exec())
